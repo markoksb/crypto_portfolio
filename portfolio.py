145c3f75eea8ded6d6bf28b748b97c439d2f9b9c
Marko <marko.brunzel@proton.me>
2024-10-29 11:53:55 +0100

    added add coin to portfolio functionality

--- portfolio.py
+++ portfolio.py
@@ -1,7 +1,28 @@
 from flask import render_template, session, request, redirect
 import cs50
+import currencies
+from error import apology
 
 db = cs50.SQL("sqlite:///portfolio.db")
+
+def add_coin_to_portfolio():
+    if request.method == "POST":
+        pid = request.form.get("pid")
+        db.execute("INSERT INTO portfolio_currency (portfolio_id, cryptocurrency_id, quantity, price) VALUES (?, ?, ?, ?)",
+                    pid, request.form.get("cid"), request.form.get("amount"), request.form.get("price"))
+        return redirect(f"/portfolio?pid={pid}")
+    else:
+        pid = request.args.get("pid")
+        currency_id = request.args.get("cid")
+        print(f"c: {currency_id}")
+        if currency_id == None:
+            # get coins from the database
+            coin_list = currencies.get_coinlist_from_db()
+            print(f"c: {coin_list}")
+            return render_template("currencies.html", coins=coin_list, pid=pid, add_coin=True)
+        else:
+            return render_template("add_currency.html", pid=pid, cid=currency_id)
+
 
 def get_users_portfolios(userid:int):
     return db.execute("SELECT * FROM portfolios WHERE user_id = ?", userid)
@@ -24,28 +45,62 @@ def create():
     
     return render_template("portfolio_new.html")
 
+class crypto_coin(object):
+    def __init__(self, id, icon_url, symbol, name, quantity, price, current_price):
+        self.id = id
+        self.icon_url = icon_url
+        self.symbol = symbol
+        self.name = name
+        self.quantity = quantity
+        self.price = price
+        self.current_price = current_price
+
 
 def portfolio():
     """
         
     """
+    coinlist = []
+    exists = False
+    if session["user_id"] == None:
+        return apology("You're not logged in.", 400)
+    portfolio_id = request.args.get("pid")
+    if portfolio_id == None:
+        pass
     portfolios = get_users_portfolios(session["user_id"])
-    currencies = ""
-    return render_template("portfolio.html", portfolios=portfolios, currencies=currencies)
+    if portfolios == None:
+        pass
+    coins = db.execute("SELECT * FROM portfolio_currency INNER JOIN currencies ON cryptocurrency_id = currencies.id WHERE portfolio_id = ? ", portfolios[0]["id"])
+    for coin in coins:
+        for cl_coin in coinlist:
+            if cl_coin.id == coin["id"]:
+                cl_coin.price = (cl_coin.price + coin["price"]) / 2
+                cl_coin.quantity += coin["quantity"]
+                exists = True
+
+        if exists != True:                
+            ccoin = crypto_coin(id=coin["id"], icon_url=coin["icon_url"], symbol=coin["symbol"], name=coin["name"], quantity=coin["quantity"], price=coin["price"], current_price=coin["current_price"])
+            coinlist.append(ccoin)
+        exists = False
+
+
+    # else:
+    #     for portfolio in portfolios:
+    #         if portfolio["id"] == portfolio_id:   
+    #             coins = db.execute("SELECT * FROM currencies WHERE id = ?", portfolio[""])
+    return render_template("portfolio.html", portfolios=portfolios, coins=coinlist, pid=portfolio_id)
 
 # CREATE TABLE portfolios (
 # id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 # user_id INTEGER,
 # name TEXT,
-# FOREIGN KEY (user_id) REFERENCES Users(id)
+# FOREIGN KEY (user_id) REFERENCES users(id)
 # );
 
 # CREATE TABLE portfolio_currency (
+# id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 # portfolio_id INTEGER NOT NULL,
 # cryptocurrency_id INTEGER NOT NULL,
 # quantity REAL NOT NULL,
 # price REAL NOT NULL,
-# PRIMARY KEY (portfolio_id, cryptocurrency_id),
-# FOREIGN KEY (portfolio_id) REFERENCES Portfolios(id),
-# FOREIGN KEY (cryptocurrency_id) REFERENCES Cryptocurrencies(id)
 # );
